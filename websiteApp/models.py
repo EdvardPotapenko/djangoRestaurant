0610ef52bcfb10e722b7da2adf4258a6f0e5c402
Edvard Potapenko <you@example.com>
2021-01-26 14:57:40 +0200

    added dish migration and model; added presentation in view for dishes

--- websiteApp/models.py
+++ websiteApp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+import os
+from uuid import uuid4
 # Create your models here.
 
 
@@ -11,3 +12,21 @@ class Category(models.Model):
 
     def __str__(self):
         return f'{self.title} : {self.category_order}'
+
+
+class Dish(models.Model):
+
+    def get_file_name_dishes(self, filename):
+        ext = filename.split('.')[-1]
+        filename = f'{uuid4()}​​.{ext}'
+        return os.path.join('images/dishes/', filename)
+
+    title = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    is_visible = models.BooleanField(default=True)
+    description = models.CharField(max_length=300, null=True)
+    photo = models.ImageField(upload_to=get_file_name_dishes)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.title} : {self.category}'
--- websiteApp/views.py
+++ websiteApp/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from .models import Category, Dish
 
 # Create your views here.
 
 
 def main(request):
-    return render(request, 'index.html')
+    categories = Category.objects.filter(is_visible=True).order_by('category_order')
+    for category in categories:
+        category.dishes = Dish.objects.filter(category=category.pk)
+    return render(request, 'index.html', context={'categories': categories})
