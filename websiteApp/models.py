a1c9d124feaee4e4afb2002053261ff0c03220b0
Edvard Potapenko <you@example.com>
2021-01-26 13:54:10 +0200

    added categories model and made migration

--- websiteApp/models.py
+++ websiteApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+    category_order = models.IntegerField()
+    is_visible = models.BooleanField(default=True)
+    is_special = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.title} : {self.category_order}'
